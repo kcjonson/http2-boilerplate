3088ddc7a47140f504bda46b9038ae1224f3db8a
Kevin Jonson <kevin.jonson@redfin.com>
2015-08-21 10:28:53 -0700

    now using express

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var koala = require('koala');
+var express = require('express')
 var fs = require('fs');
 var spdy = require('spdy')
 
+var app = express()
 
+app.get('/', function (req, res) {
+	res.sendFile('./app.html', { root : __dirname})
+})
 
+app.get('/conbats.gif', function (req, res) {
+	res.sendFile('./conbats.gif', { root : __dirname})
+})
 
+app.get('/images/:id', function (req, res) {
+	res.sendFile('./images/' + req.params.id, { root : __dirname})
+})
 
+var options = {
+  key: fs.readFileSync('ssl.key'),
+  cert: fs.readFileSync('ssl.crt')
+};
 
-var app = koala();
-
-app.use(function* () {
-	yield this.fileServer.send('app.html');
-});
-
-var server = spdy.createServer({
-    key: fs.readFileSync('ssl.key'),
-    cert: fs.readFileSync('ssl.crt'),
-}, app.callback());
-
-server.listen(3000);
+spdy.createServer(options, app).listen(8080);
